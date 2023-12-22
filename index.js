48767d6d44ac69e11dfb13887a3f143138af80d3
rezwan2230 <rezwan2230@gmail.com>
2023-12-22 18:09:41 +0600

    added delete functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvdjbig.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const toDoesCollection = client.db("taskManagement").collection("toDoes");
@@ -38,11 +38,12 @@ async function run() {
       res.send(result)
     })
 
-
-
-
-
-
+    app.delete('/toDoes/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toDoesCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
     
